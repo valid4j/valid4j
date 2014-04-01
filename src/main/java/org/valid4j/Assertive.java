84e3f689564f096b7685d2d38312a481dcc332b5
Patrik Helsing <patrik.helsing@gmail.com>
2014-04-01 22:33:19 +0200

    Improve JavaDoc on public API classes

--- src/main/java/org/valid4j/Assertive.java
+++ src/main/java/org/valid4j/Assertive.java
@@ -5,6 +5,17 @@ import org.hamcrest.*;
 /**
  * Entry point for the global assertive policy.
  * 
+ * Violations indicate the presence of a programming error. 
+ * Never try to catch and recover from a programming error. Rather
+ * a reasonable global approach would be to return a HTTP 500 response, 
+ * restart the application or similar.
+ * 
+ * All error messages are {@link java.lang.String#format format strings} as defined by {@link java.lang.String}.
+ * 
+ * Example:
+ * <code>
+ *   require(i > 0, "The value must be greater than zero: %d", i);
+ * </code>
  */
 public class Assertive {
 	
@@ -14,49 +25,130 @@ public class Assertive {
 	private static AssertivePolicy requirePolicy = new CheckAssertivePolicy(new RequireViolationPolicy());
 	private static AssertivePolicy ensurePolicy = new CheckAssertivePolicy(new EnsureViolationPolicy());
 	private static UnreachablePolicy neverGetHerePolicy = new NeverGetHerePolicy();
-		
+	
+	/**
+	 * Precondition that clients are required to fulfill.
+	 * Violations are considered to be programming errors, 
+	 * on the clients part.
+	 * 
+	 * @param condition the condition to check
+	 */
 	public static void require(boolean condition) {
 	  requirePolicy.check(condition, NULL_MESSAGE);
 	}
 	
+	/**
+   * Precondition that clients are required to fulfill.
+   * Violations are considered to be programming errors, 
+   * on the clients part.
+	 * 
+	 * @param o        the object to check
+	 * @param matcher  the matcher that the given object must satisfy
+	 * @return         the validated object
+	 */
 	public static <T> T require(T o, Matcher<?> matcher) {
 	  requirePolicy.check(o, matcher);
 		return o;
 	}
 	
+	/**
+	 * Precondition that clients are required to fulfill.
+   * Violations are considered to be programming errors, 
+   * on the clients part.
+	 * 
+	 * @param condition  the condition to check
+	 * @param msg        message {@link java.lang.String#format format string}
+	 * @param values     values passed into the msg format string
+	 */
 	public static void require(boolean condition, String msg, Object... values) {
 	  requirePolicy.check(condition, msg, values);
 	}
 
+	/**
+	 * Postcondition that supplier are supposed to ensure.
+	 * Violations are considered to be a programming error,
+	 * on the suppliers part.
+	 * 
+	 * @param condition the condition to check
+	 */
 	public static void ensure(boolean condition) {
 	  ensurePolicy.check(condition, NULL_MESSAGE);
 	}
 
+  /**
+   * Postcondition that supplier are supposed to ensure.
+   * Violations are considered to be programming errors, 
+   * on the suppliers part.
+   * 
+   * @param o        the object to check
+   * @param matcher  the matcher that the given object must satisfy
+   * @return         the validated object
+   */
 	public static <T> T ensure(T o, Matcher<?> matcher) {
 	  ensurePolicy.check(o, matcher);
 		return o;
 	}
 
+	/**
+   * Postcondition that supplier are supposed to ensure.
+   * Violations are considered to be programming errors, 
+   * on the suppliers part.
+   * 
+   * @param condition  the condition to check
+   * @param msg        message {@link java.lang.String#format format string}
+   * @param values     values passed into the msg format string
+	 */
 	public static void ensure(boolean condition, String msg, Object... values) {
 	  ensurePolicy.check(condition, msg, values);
 	}
 
+	/**
+	 * Unreachable code have been reached. 
+	 * This is considered to be a programming error.
+	 */
 	public static void neverGetHere() {
 	  neverGetHerePolicy.neverGetHere(NO_CAUSE, NULL_MESSAGE);
 	}
 
+	/**
+	 * Unreachable code have been reached. This is 
+	 * considered to be a programming error.
+	 * 
+	 * @param msg  descriptive message
+	 */
 	public static void neverGetHere(String msg) {
 	  neverGetHerePolicy.neverGetHere(NO_CAUSE, msg);
 	}
 
+	/**
+	 * Unreachable code have been reached. This is 
+	 * considered to be a programming error.
+	 * 
+   * @param msg        message {@link java.lang.String#format format string}
+   * @param values     values passed into the msg format string
+	 */
 	public static void neverGetHere(String msg, Object... values) {
 	  neverGetHerePolicy.neverGetHere(NO_CAUSE, msg, values);
 	}
-		
+	
+	/**
+	 * Unexpected exception have been caught. This is considered
+	 * to be a programming error.
+	 * 
+	 * @param t  the throwable being unexpectedly caught.
+	 */
 	public static void neverGetHere(Throwable t) {
 	  neverGetHerePolicy.neverGetHere(t, NULL_MESSAGE);
 	}
 
+	/**
+   * Unexpected exception have been caught. This is considered
+   * to be a programming error.
+   * 
+   * @param t       the throwable being unexpectedly caught.
+   * @param msg     message {@link java.lang.String#format format string}
+   * @param values  values passed into the msg format string
+	 */
 	public static void neverGetHere(Throwable t, String msg, Object... values) {
 	  neverGetHerePolicy.neverGetHere(t, msg, values);
 	}
--- src/main/java/org/valid4j/Validation.java
+++ src/main/java/org/valid4j/Validation.java
@@ -2,15 +2,43 @@ package org.valid4j;
 
 import org.hamcrest.Matcher;
 
+/**
+ * Entry point for validation methods. 
+ * 
+ * Use these methods to throw recoverable exceptions if validation fails.
+ * Recoverable exceptions are exceptions that a correct program can and 
+ * should catch and recover from. (Recoverable exceptions may be checked
+ * or unchecked.)
+ */
 public class Validation {
 
-  public static <X extends Throwable> void validate(boolean condition, X exception) throws X {
+  /**
+   * Validate the boolean expression that is supposed to be true. 
+   * If not, a recoverable exception will be thrown that the client
+   * is supposed to catch and recover from.
+   * 
+   * @param condition   the condition to validate. Supposed to be true.
+   * @param exception   the recoverable exception to be thrown if validation fails.
+   * @throws X          the recoverable exception, if validation fails
+   */
+  public static <X extends Exception> void validate(boolean condition, X exception) throws X {
     if (!condition) {
       throw exception;
     }
   }
 
-  public static <T, X extends Throwable> T validate(T o, Matcher<?> matcher, X exception) throws X {
+  /**
+   * Validate the given object that is supposed to satisfy the matcher.
+   * If not, a recoverable exception will be thrown that the client
+   * is supposed to catch and recover from.
+   * 
+   * @param o         the object to validate
+   * @param matcher   the matcher that the object must satisfy
+   * @param exception the recoverable exception to be thrown if validation fails.
+   * @return          the validated object, if valid
+   * @throws X        the recoverable exception, if validation fails
+   */
+  public static <T, X extends Exception> T validate(T o, Matcher<?> matcher, X exception) throws X {
     if (!matcher.matches(o)) {
       throw exception;
     }
