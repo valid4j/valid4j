c64be6cdb5a171dec7678fa82adaa66a91290173
Patrik Helsing <patrik.helsing@gmail.com>
2014-04-27 20:01:27 +0200

    Added some tests for message formatting

--- src/test/java/org/valid4j/MessageBuilderBehavior.java
+++ src/test/java/org/valid4j/MessageBuilderBehavior.java
@@ -15,13 +15,25 @@ public class MessageBuilderBehavior {
   
   @Test
   public void shouldBuildMessageFromInvalidFormat() {
-    Object m = MessageBuilder.from("Some %s %d", "message", "with invalid formatting", 4);
+    Object m = MessageBuilder.from("Some %s %d", "message", "with invalid formatting");
     assertThat(m.toString(), containsString("Some %s %d"));
     assertThat(m.toString(), containsString("message"));
     assertThat(m.toString(), containsString("with invalid formatting"));
-    assertThat(m.toString(), containsString("4"));
   }
 
+  @Test
+  public void shouldBuildMessageFromTooFewArguments() {
+    Object m = MessageBuilder.from("Some %s %s", "message with too few arguments");
+    assertThat(m.toString(), containsString("Some %s %s"));
+    assertThat(m.toString(), containsString("message with too few arguments"));
+  }
+
+  @Test
+  public void shouldBuildMessageFromTooManyArguments() {
+    Object m = MessageBuilder.from("Some %s", "message", "with too many arguments");
+    assertThat(m.toString(), containsString("Some message"));
+  }
+  
   @Test
   public void shouldBuildMessageFromNullFormat() {
     Object m = MessageBuilder.from(null);
@@ -35,7 +47,13 @@ public class MessageBuilderBehavior {
     assertThat(m.toString(), containsString("argument"));
     assertThat(m.toString(), containsString("4"));
   }
-  
+
+  @Test
+  public void shouldBuildMessageWithNullArguments() {
+    Object m = MessageBuilder.from("Some %s %d %b", (Object)null, (Object)null, (Object)null);
+    assertThat(m.toString(), containsString("Some null null false"));
+  }
+
   @Test
   public void shouldBuildMismatchMessage() {
     Object m = MessageBuilder.withMismatchOf(3, not(equalTo(2)));
