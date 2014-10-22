2f06d487d831e59f58cee3ed78325ca723f5895c
Patrik Helsing <patrik.helsing@gmail.com>
2014-10-22 21:22:32 +0200

    Added convenience matchers

--- src/main/java/org/valid4j/matchers/ArgumentMatchers.java
+++ src/main/java/org/valid4j/matchers/ArgumentMatchers.java
@@ -2,15 +2,27 @@ package org.valid4j.matchers;
 
 import org.hamcrest.Matcher;
 
-import static org.hamcrest.CoreMatchers.describedAs;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
 
 /**
  * Common convenience matchers for arguments
  */
 public class ArgumentMatchers {
 
-  public static Matcher<Object> notNullArg(final String argumentName) {
-    return describedAs("%0 not null", notNullValue(), argumentName);
+  public static Matcher<Object> notNullArg() {
+    return describedAs("not null", notNullValue());
+  }
+
+  public static Matcher<Object> notNullArg(final String name) {
+    return describedAs("%0 not null", notNullValue(), name);
+  }
+
+  public static Matcher<String> notEmptyArg() {
+    return describedAs("not empty", not(isEmptyOrNullString()));
+  }
+
+  public static Matcher<String> notEmptyArg(final String name) {
+    return describedAs("%0 not empty", not(isEmptyOrNullString()), name);
   }
 }
--- src/test/java/org/valid4j/matchers/ArgumentMatcherBehavior.java
+++ src/test/java/org/valid4j/matchers/ArgumentMatcherBehavior.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertThat;
+import static org.valid4j.matchers.ArgumentMatchers.notEmptyArg;
 import static org.valid4j.matchers.ArgumentMatchers.notNullArg;
 
 /**
@@ -14,12 +15,22 @@ import static org.valid4j.matchers.ArgumentMatchers.notNullArg;
 public class ArgumentMatcherBehavior {
 
   @Test
-  public void shouldMatchNotNullArguments() {
+  public void shouldMatchNotNullArgument() {
     assertThat(new Object(), notNullArg("myArgument"));
     assertThat(null, not(notNullArg("someArgument")));
 
     StringDescription description = new StringDescription();
     notNullArg("my-parameter").describeTo(description);
     assertThat(description.toString(), equalTo("\"my-parameter\" not null"));
+  }
+
+  @Test
+  public void shouldMatchNotEmptyArgument() {
+    assertThat(" ", notEmptyArg("field1"));
+    assertThat("", not(notEmptyArg("field2")));
+
+    StringDescription description = new StringDescription();
+    notEmptyArg("field3").describeTo(description);
+    assertThat(description.toString(), equalTo("\"field3\" not empty"));
   }
 }
