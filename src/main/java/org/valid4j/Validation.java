e1ab9a1bdabe94dce01f30347b34b48dd1c24795
Patrik Helsing <patrik.helsing@gmail.com>
2014-10-23 22:41:27 +0200

    Added method to validate given an exception class with a generated error message.

--- src/main/java/org/valid4j/Validation.java
+++ src/main/java/org/valid4j/Validation.java
@@ -1,8 +1,8 @@
 package org.valid4j;
 
-import static org.valid4j.Assertive.neverGetHere;
-
 import org.hamcrest.Matcher;
+
+import static org.valid4j.Assertive.neverGetHere;
 
 /**
  * Entry point for validation methods. 
@@ -17,15 +17,15 @@ public class Validation {
   private Validation() {
     neverGetHere("Prevent instantiation");
   }
-  
+
   /**
-   * Validate the boolean expression that is supposed to be true. 
+   * Validate the boolean expression that is supposed to be true.
    * If not, a recoverable exception will be thrown that the client
    * is supposed to catch and recover from.
-   * 
-   * @param condition   the condition to validate. Supposed to be true.
-   * @param exception   the recoverable exception to be thrown if validation fails.
-   * @throws X          the recoverable exception, if validation fails
+   *
+   * @param condition the condition to validate. Supposed to be true.
+   * @param exception the recoverable exception to be thrown if validation fails.
+   * @throws X the recoverable exception, if validation fails
    */
   public static <X extends Exception> void validate(boolean condition, X exception) throws X {
     if (!condition) {
@@ -33,16 +33,22 @@ public class Validation {
     }
   }
 
+  public static <X extends Exception> void validate(boolean condition, Class<X> exceptionClass) throws X {
+    if (!condition) {
+      throw ExceptionFactories.builder(exceptionClass).newInstance(null);
+    }
+  }
+
   /**
    * Validate the given object that is supposed to satisfy the matcher.
    * If not, a recoverable exception will be thrown that the client
    * is supposed to catch and recover from.
-   * 
+   *
    * @param o         the object to validate
    * @param matcher   the matcher that the object must satisfy
    * @param exception the recoverable exception to be thrown if validation fails.
-   * @return          the validated object, if valid
-   * @throws X        the recoverable exception, if validation fails
+   * @return the validated object, if valid
+   * @throws X the recoverable exception, if validation fails
    */
   public static <T, X extends Exception> T validate(T o, Matcher<?> matcher, X exception) throws X {
     if (!matcher.matches(o)) {
@@ -51,4 +57,14 @@ public class Validation {
     return o;
   }
 
+  public static <T, X extends Exception> T validate(T o, Matcher<?> matcher, Class<X> exceptionClass) throws X {
+    if (!matcher.matches(o)) {
+      throw ExceptionFactories.builder(exceptionClass).newInstance(withMessage(o, matcher));
+    }
+    return o;
+  }
+
+  private static String withMessage(Object actual, Matcher<?> matcher) {
+    return Message.withMismatchMessageOf(actual, matcher);
+  }
 }
