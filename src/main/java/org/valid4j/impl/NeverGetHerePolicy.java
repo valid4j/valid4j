0dff56167b80d6c581afca2f8ef5787e0c078d63
Patrik Helsing <patrik.helsing@gmail.com>
2014-11-20 19:31:24 +0100

    Use the tracking policy to track thrown never-get-here errors

--- src/main/java/org/valid4j/impl/NeverGetHerePolicy.java
+++ src/main/java/org/valid4j/impl/NeverGetHerePolicy.java
@@ -11,21 +11,14 @@ import static org.valid4j.Message.withFormattedMessage;
 /**
  * Policy for handling unreachable code by throwing an unrecoverable exception.
  */
-public class NeverGetHerePolicy implements UnreachablePolicy {
-
-  private final Queue<ContractViolation> violations;
+public class NeverGetHerePolicy extends TrackingViolationPolicy implements UnreachablePolicy {
 
   public NeverGetHerePolicy(Queue<ContractViolation> violations) {
-    this.violations = violations;
+    super(violations);
   }
 
   @Override
   public void neverGetHere(Throwable cause, String msg, Object... values) {
     throw tracked(new NeverGetHereViolation(cause, withFormattedMessage(msg, values)));
-  }
-
-  private NeverGetHereViolation tracked(NeverGetHereViolation neverGetHereViolation) {
-    violations.offer(neverGetHereViolation);
-    return neverGetHereViolation;
   }
 }
