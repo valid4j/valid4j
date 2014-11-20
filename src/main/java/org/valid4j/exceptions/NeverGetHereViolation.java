e8cedadd89c38dddd58147439e6eb197a654a3ae
Patrik Helsing <patrik.helsing@gmail.com>
2014-11-20 16:55:01 +0100

    Remove unused constructor

--- src/main/java/org/valid4j/exceptions/NeverGetHereViolation.java
+++ src/main/java/org/valid4j/exceptions/NeverGetHereViolation.java
@@ -8,10 +8,6 @@ public class NeverGetHereViolation extends ContractViolation {
 
 	private static final long serialVersionUID = -7898390791997251693L;
 
-	public NeverGetHereViolation(String msg) {
-		super(msg);
-	}
-		
 	public NeverGetHereViolation(Throwable t, String msg) {
 		super(t, msg);
 	}
